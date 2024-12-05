12b9278e1e95a6ea9e4bbfcf6cd8b04bd11c2298
atg2431 <anshuman.biswal.cd.eee23@itbhu.ac.in>
2024-12-05 12:28:32 +0530

    added the initial files

--- research/echo_bot.py
+++ research/echo_bot.py
@@ -1,2 +1,15 @@
 import logging
-from aiogram import Bot, Dispatcher, executer, types
+from aiogram import Bot, Dispatcher, types
+from aiogram import executer
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+# print(TELEGRAM_BOT_TOKEN)
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token = TELEGRAM_BOT_TOKEN)
+dp = Dispatcher(bot)
