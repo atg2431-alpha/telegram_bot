77b205d9ec5ffc5282ce7ee4b6d5b657191755f5
atg2431 <anshuman.biswal.cd.eee23@itbhu.ac.in>
2024-12-05 13:32:31 +0530

    changed the aiogram code to latest version

--- research/echo_bot.py
+++ research/echo_bot.py
@@ -1,15 +1,31 @@
-import logging
-from aiogram import Bot, Dispatcher, types
-from aiogram import executer
 from dotenv import load_dotenv
+import asyncio
+import logging
+import sys
 import os
+
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 
 load_dotenv()
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
-# print(TELEGRAM_BOT_TOKEN)
+dp = Dispatcher()
 
-logging.basicConfig(level=logging.INFO)
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
 
-bot = Bot(token = TELEGRAM_BOT_TOKEN)
-dp = Dispatcher(bot)
+async def main() -> None:
+    bot = Bot(token=TELEGRAM_BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
